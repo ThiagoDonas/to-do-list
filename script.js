cad712e3ef9afda1e06c69fa5bd451fc969e9707
ThiagoDonas <88470357+ThiagoDonas@users.noreply.github.com>
2023-04-18 12:42:07 -0300

    correction row

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const submit = forms.elements.submit;
 submit.onclick = function (e) {
   e.preventDefault();
   const tabela = document.getElementById('table');
-  const row = table.insertRow(0);
+  const row = table.insertRow(1);
   const cell1 = row.insertCell(0);
   const cell2 = row.insertCell(1);
   cell1.innerHTML = nome.value;
