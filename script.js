1bc861685a14b26b373fa628595b983ed9e3d6f9
ThiagoDonas <88470357+ThiagoDonas@users.noreply.github.com>
2023-04-19 12:26:41 -0300

    refatoração do codigo

--- script.js
+++ script.js
@@ -1,50 +1,91 @@
-function createActivity() {
-  const tabela = document.getElementById('table');
-  const row = table.insertRow(2);
-  const cell1 = row.insertCell(0);
-  const cell2 = row.insertCell(1);
-  const cell3 = row.insertCell(2);
-  const cell4 = row.insertCell(3);
-  cell1.innerHTML = nome.value;
-  cell2.innerHTML = date.value;
-  cell3.innerHTML = `<input class= 'button'  id= ${nome.value}${date.value} type="submit" value="Editar"/>`;
-  cell4.innerHTML = `<input class= 'button'  id= ${nome.value}${date.value} type="submit" value="Excluir"/>`;
-}
-
-function actionButtonTable(input) {
-  if (input.value == 'Excluir') {
-    const tr = document.getElementById(`${input.id}`).parentElement
-      .parentElement;
-    tr.remove();
-  }
-  if (input.value == 'Editar') {
-    const tr = document.getElementById(`${input.id}`).parentElement
-      .parentElement;
-    console.log(forms.elements.Atividade);
-    console.log(tr.children[0]);
-    forms.elements.Atividade.value = tr.children[0].textContent;
-    forms.elements.Data.value = tr.children[1].textContent;
-    //tr.remove();
-  }
-}
-
 const forms = document.getElementById('myform');
-const nome = forms.elements.Atividade;
+const name = forms.elements.Atividade;
 const date = forms.elements.Data;
 const submit = forms.elements.submit;
+let tarefas = [];
+let tabela = document.getElementById('corpotabela');
 
-submit.onclick = function (e) {
-  e.preventDefault();
-  const exist = document.getElementById(`${nome.value}${date.value}`);
-  if (exist == null) {
-    createActivity();
-  } else {
-  }
-  const buttontable = document.querySelectorAll('.button');
-  buttontable.forEach(function (element) {
-    element.onclick = function (e) {
-      const input = e.srcElement;
-      actionButtonTable(input);
-    };
+function adicionarTarefas(name, date) {
+  console.log(name, date);
+  //tarefas = [...tarefas, { name, date }];
+  tarefas.push({ name, date });
+  localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+function recuperarTarefas() {
+  const temp = JSON.parse(localStorage.getItem('tarefas'));
+  temp.forEach((tarefa) => {
+    adicionarTarefas(tarefa.name, tarefa.date);
   });
-};
+  //tarefas = [...JSON.parse(localStorage.getItem('tarefas'))]
+}
+
+function editarTarefa(index) {
+  const temp = tarefas[index];
+  forms.elements.name.value = temp.name;
+  forms.elements.date.value = temp.date;
+}
+
+function salvarTarefa(index, name, date) {
+  tarefas[index] = { name, date };
+  limpaForms();
+}
+
+function limpaForms() {
+  forms.elements.name.value = null;
+  forms.elements.date.value = null;
+}
+
+function removerLista(index) {
+  tarefas.splice(index, 1);
+}
+
+function cretaeRow() {
+  return document.createElement('tr');
+}
+function createColum() {
+  return document.createElement('td');
+}
+
+function createButtonEdite() {
+  const button = document.createElement('button');
+  button.innerText = 'Editar';
+  button.id = 'editar';
+}
+
+function creteButtonExcluir() {
+  const button = document.createElement('button');
+  button.innerText = 'Excluir';
+  button.id = 'excluir';
+}
+
+function rendereizarTabela() {
+  tabela.innerHTML = ' ';
+  console.log(tarefas);
+  tarefas.forEach((tarefa) => {
+    const linha = cretaeRow();
+    const colunaName = createColum();
+    const colunaDate = createColum();
+    const buttonEdite = createButtonEdite();
+    const buttonExcluir = creteButtonExcluir();
+    colunaName.innerText = tarefa.name;
+    colunaDate.innerText = tarefa.date;
+
+    linha.appendChild(colunaName);
+    linha.appendChild(colunaDate);
+    linha.appendChild(buttonEdite);
+    linha.appendChild(buttonExcluir);
+    tabela.appendChild(linha);
+  });
+}
+
+forms.addEventListener('submit', (e) => {
+  e.preventDefault();
+  adicionarTarefas(forms.elements.name.value, forms.elements.date.value);
+  rendereizarTabela();
+  limpaForms();
+});
+
+if (tarefas.length > 0) {
+  rendereizarTabela();
+}
