c335c3f3c06285698e8444f4735b234354c4f5cb
ThiagoDonas <88470357+ThiagoDonas@users.noreply.github.com>
2023-04-19 10:54:04 -0300

    insert functions

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+function createAtividade() {
+  const tabela = document.getElementById('table');
+  const row = table.insertRow(2);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  const cell4 = row.insertCell(3);
+  cell1.innerHTML = nome.value;
+  cell2.innerHTML = date.value;
+  cell3.innerHTML = `<input class= 'button'  id= ${nome.value}${date.value} type="submit" value="Editar"/>`;
+  cell4.innerHTML = `<input class= 'button'  id= ${nome.value}${date.value} type="submit" value="Excluir"/>`;
+}
+
+function actionButtonTable(input) {
+  if (input.value == 'Excluir') {
+    const tr = document.getElementById(`${input.id}`).parentElement
+      .parentElement;
+    tr.remove();
+  }
+  if (input.value == 'Editar') {
+    const tr = document.getElementById(`${input.id}`).parentElement
+      .parentElement;
+    console.log(forms.elements.Atividade);
+    console.log(tr.children[0]);
+    forms.elements.Atividade.value = tr.children[0].textContent;
+    forms.elements.Data.value = tr.children[1].textContent;
+    //tr.remove();
+  }
+}
+
 const forms = document.getElementById('myform');
 const nome = forms.elements.Atividade;
 const date = forms.elements.Data;
@@ -7,35 +37,18 @@ submit.onclick = function (e) {
   e.preventDefault();
   const exist = document.getElementById(`${nome.value}${date.value}`);
   if (exist == null) {
-    const tabela = document.getElementById('table');
-    const row = table.insertRow(2);
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
-    const cell3 = row.insertCell(2);
-    const cell4 = row.insertCell(3);
-    cell1.innerHTML = nome.value;
-    cell2.innerHTML = date.value;
-    cell3.innerHTML = `<input class= 'button'  id= ${nome.value}${date.value} type="submit" value="Editar"/>`;
-    cell4.innerHTML = `<input class= 'button'  id= ${nome.value}${date.value} type="submit" value="Excluir"/>`;
+    createAtividade();
   } else {
+    
   }
+};
+
+window.onclick = function () {
   const buttontable = document.querySelectorAll('.button');
   buttontable.forEach(function (element) {
     element.onclick = function (e) {
       const input = e.srcElement;
-      if (input.value == 'Excluir') {
-        const tr = document.getElementById(`${input.id}`).parentElement
-          .parentElement;
-        tr.remove();
-      }
-      if (input.value == 'Editar') {
-        const tr = document.getElementById(`${input.id}`).parentElement
-          .parentElement;
-        console.log(forms.elements.Atividade);
-        console.log(tr.children[0]);
-        forms.elements.Atividade.value = tr.children[0].textContent;
-        forms.elements.Data.value = tr.children[1].textContent;
-      }
+      actionButtonTable(input);
     };
   });
 };
